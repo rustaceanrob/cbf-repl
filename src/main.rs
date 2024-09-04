8d1f734e90768945efd96b11e229151f85ff5558
Rob N <rob.netzke@gmail.com>
2024-09-03 19:04:27 -1000

    lib: starter template

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use bdk_wallet::*;
+use bdk_kyoto::*;
+use tokio::*;
+
+const RECEIVE: &str = "tr([7d94197e/86'/1'/0']tpubDCyQVJj8KzjiQsFjmb3KwECVXPvMwvAxxZGCP9XmWSopmjW3bCV3wD7TgxrUhiGSueDS1MU5X1Vb1YjYcp8jitXc5fXfdC1z68hDDEyKRNr/0/*)";
+const CHANGE: &str = "tr([7d94197e/86'/1'/0']tpubDCyQVJj8KzjiQsFjmb3KwECVXPvMwvAxxZGCP9XmWSopmjW3bCV3wD7TgxrUhiGSueDS1MU5X1Vb1YjYcp8jitXc5fXfdC1z68hDDEyKRNr/1/*)";
+const CMD_RECV: &str = "receive";
+const CMD_BALANCE: &str = "balance";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+
+
+    Ok(())
+}
