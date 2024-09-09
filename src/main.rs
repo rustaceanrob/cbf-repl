3f2a13ebcca733220574ce47a78cb2563a058b77
Rob N <rob.netzke@gmail.com>
2024-09-09 08:30:55 -1000

    doc: add code comments

--- src/main.rs
+++ src/main.rs
@@ -10,46 +10,58 @@ const CMD_RECV: &str = "address";
 const CMD_BALANCE: &str = "balance";
 const CMD_SHUTDOWN: &str = "shutdown";
 
+// Read a line from the terminal input
 async fn read_lines(stdin: &mut Lines<BufReader<Stdin>>) -> Option<String> {
     stdin.next_line().await.ok().unwrap_or(None)
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // Better console logging
     let subscriber = tracing_subscriber::FmtSubscriber::new();
     tracing::subscriber::set_global_default(subscriber)?;
 
+    // Set up the terminal input
     let stdin = tokio::io::stdin();
     let reader = BufReader::new(stdin);
     let mut lines = reader.lines();
 
+    // Open a connection to the wallet database
     let mut conn = Connection::open(".bdk-wallet.sqlite")?;
 
+    // Attempt to load the wallet from the database connection. 
     let wallet_opt = Wallet::load()
         .descriptor(KeychainKind::External, Some(RECEIVE))
         .descriptor(KeychainKind::Internal, Some(CHANGE))
-        .extract_keys()
         .check_network(Network::Signet)
         .load_wallet(&mut conn)?;
 
     let mut wallet = match wallet_opt {
+        // We found an existing wallet
         Some(wallet) => wallet,
+        // No wallet exists, so we create one here
         None => Wallet::create(RECEIVE, CHANGE)
             .network(Network::Signet)
             .create_wallet(&mut conn)?,
     };
 
     let (mut node, mut client) = LightClientBuilder::new(&wallet)
+        // When recovering a wallet, specify the height to start scanning
         .scan_after(170_000)
+        // The number of remote connections to maintain
         .connections(1)
         .build()?;
 
+    // Run the node on a separate task
     tokio::task::spawn(async move { node.run().await });
 
+    // Log events that are issued from the node to the user
     let logger = TraceLogger::new();
 
+    // Wait for an update for the wallet from the node
     let wallet_update = client.update(&logger).await;
 
+    // Apply the update and write it to the database
     if let Some(update) = wallet_update {
         wallet.apply_update(update)?;
         wallet.persist(&mut conn)?;
@@ -58,12 +70,14 @@ async fn main() -> anyhow::Result<()> {
     loop {
         tracing::info!("Awaiting the next command...");
         select! {
+            // Wait for new blocks and apply any updates
             update = client.update(&logger) => {
                 if let Some(update) = update {
                     wallet.apply_update(update)?;
                     wallet.persist(&mut conn)?;                    
                 }
             },
+            // Wait for a command from the user
             lines = read_lines(&mut lines) => {
                 if let Some(line) = lines {
                     match line.as_str() {
